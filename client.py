610ce48ed2d90cac3005f2aea9529c2337e09603
icaijy <caijunyi08@outlook.com>
2021-01-21 11:04:11 +0800

    finish basic features

--- client.py
+++ client.py
@@ -1,19 +1,45 @@
 import socket
+from threading import Thread
+import os
 
-# 创建 socket 对象
+def exit():
+    '''
+    exit
+    :return: None
+    '''
+    os._exit(1)
+
+def moreFunc(**kwargs):
+    threads = []
+    for func, args in kwargs.items():
+        threads.append(Thread(target=globals()[func],args=args))
+    for t in threads:
+        t.start()
+
+
+# socket object
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# set prot
-port = 9999
-ip = socket.gethostbyname(socket.gethostname())
-# connect
-s.connect((ip, port))
+# set port
+def connect(ip=socket.gethostbyname(socket.gethostname()),port=9999):
+    # connect
+    s.connect((ip, port))
+    return None
+
 
 # recieve
-while True:
-    msg = s.recv(1024)
-    if msg:
-        print("from server: "+msg.decode('utf-8'))
-    else:
-        print("server close!")
-        exit()
+
+def receive():
+    while True:
+        try:
+            msg = s.recv(1024)
+            if msg:
+                print("from server: "+msg.decode('utf-8'))
+        except ConnectionResetError:
+            print('server close')
+            os._exit(1)
+
+
+connect(ip=input("server's ip:"))
+#keyboard.add_hotkey('ctrl+q',exit)
+moreFunc(receive=())
--- server.py
+++ server.py
@@ -1,40 +1,64 @@
 import socket
-
 from threading import Thread
+import os
+import keyboard
+
+###################################################################
+global client, addr
+c = []
+def exit():
+    '''
+    exit
+    :return: None
+    '''
+    print("Bye\n")
+    os._exit(1)
+
+
 def moreFunc(**kwargs):
+    '''
+    run more functions
+    :param kwargs:
+    :return:
+    '''
     threads = []
     for func, args in kwargs.items():
         threads.append(Thread(target=globals()[func],args=args))
     for t in threads:
         t.start()
 
-'''
-AF_INET: IPv4
-AF_INET6: IPv6
-SOCK_STREAM: TCP 
-'''
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-def waitconnect():
+def initconnect(port=9999):
     # listen
     ip = socket.gethostbyname(socket.gethostname())
-    port = 9999
-
     print(ip)
     s.bind((ip, port))
-
-
     # largest connection
     # now will wait for connection
     s.listen(5)
     return None
 
 
+def send(msg):
+    client.send(msg.encode('utf-8'))  # send bytes
+####################################################################
+
+# socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+# if press esc, exit.
+keyboard.add_hotkey('esc',exit)
+
+initconnect()
+
+client, addr = s.accept()  # start connection
+
 while True:
-    waitconnect()
-    client,addr = s.accept()     # start connection
-    while True:
+    try:
         msg = input("send: ")
-        client.send(msg.encode('utf-8')) # send bytes
-    #client.close() # close connection
+        send(msg)  # send bytes
+    except:
+        print("no connect!")
+        client, addr = s.accept()  # start connection
+
+
